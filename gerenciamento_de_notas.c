2c84c8ea47cfa5a3b63de9bc263b27252ba37e6e
IgorYanko <99884866+IgorYanko@users.noreply.github.com>
2024-10-23 14:24:51 -0300

    Criei matriz para simular um Kvp

--- gerenciamento_de_notas.c
+++ gerenciamento_de_notas.c
@@ -2,7 +2,7 @@
 
 int registrarAlunos() // Yanko
 {
-    int i, id = 0, notasAlunos[30], nota = 0, soma;
+    int i, j, id = 0, notasAlunos[30][2], nota = 0, soma;
     
     // Título da aplicação no console
     printf("\t\t\t\t -----------------------------------\n");
@@ -13,24 +13,24 @@ int registrarAlunos() // Yanko
     
     // Laço de repetição para cadastro de até 30 alunos
     for (i = 0; i < 30; i++) {
-        printf("Escreva o id do aluno: (Digite um numero negativo pra encerrar)\n");
+        printf("\nEscreva o id do aluno: (Digite um numero negativo pra encerrar)\n");
         scanf("%d", &id);
-        
-        soma = 0; // Zera o valor de soma para não acumular os valores
     
         if(id < 1){ // Verifica se a condição de encerramento foi cumprida
             printf("Cadastro encerrado!");
-            printf("\n%d\n", notasAlunos[4]);
             return -1; // Encerra o programa (poderia ser return 0 tmb)
         }  else {
-            for (i = 0; i < 3; i++) { // Laço de repetição para as 3 notas
-                printf("\nEscreva a nota: ");
+            soma = 0; // Zera o valor de soma para não acumular os valores
+            
+            for (j = 0; j < 3; j++) { // Laço de repetição para as 3 notas
+                printf("\nEscreva a %d nota: ", j + 1);
                 scanf("%d", &nota);
                 
                 soma += nota;
             }
-            
-            notasAlunos[i] = soma; // Atribuindo a soma a uma posição do vetor
+
+            notasAlunos[i][0] = id; // Atribuindo o id a primeira linha do vetor
+            notasAlunos[i][1] = soma; // Atribuindo a soma a segunda linha do vetor
             // Não realizei a média diretamente pra facilitar na hora de calcular a individual e geral
         }  
     }
